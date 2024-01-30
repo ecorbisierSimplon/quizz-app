b78bd4aae0b584b1c5e004e5e68dce7993af7b75
Eteib Dembele <dembeleeteib@gmail.com>
2024-01-30 12:44:33 +0100

    validator

--- backend/src/users/dto/createUser.dto.ts
+++ backend/src/users/dto/createUser.dto.ts
@@ -1,7 +1,20 @@
+import { IsString, IsEmail, IsInt, Min, Max } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
   sur_name: string;
+
+  @IsString()
   first_name: string;
+
+  @IsEmail()
   email: string;
+
+  @IsString()
   key: string;
+
+  @IsInt()
+  @Min(1)
+  @Max(5)
   role: number;
 }
