2324a63f9a273728e6c5eef068f816bb6f1e967c
Eric CORBISIER <144805223+ecorbisierSimplon@users.noreply.github.com>
2024-02-01 15:33:00 +0100

    Update +page.server.ts

--- frontend/src/routes/login/+page.server.ts
+++ frontend/src/routes/login/+page.server.ts
@@ -1,26 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import type { Actions } from './$types';
-=======
-// import { json } from '@sveltejs/kit';
 import { fail } from '@sveltejs/kit';
->>>>>>> 5760273087ee00868df01f802a01aa99ccabf680
 import type { PageServerLoad, Actions } from './$types';
 const importJwt = () => import('jsonwebtoken');
-
-<<<<<<< HEAD
-import { fail } from '@sveltejs/kit';
-
-export const load: PageServerLoad + async({ cookies }) => {
-	const user = await db.getUserFromSession(cookies.get('sessionid'));
-	return { user };
-};
-=======
-import dotenv from 'dotenv';
-=======
-import { fail } from '@sveltejs/kit';
->>>>>>> f0a9c7614cc40dd07c1118b5892b820b48567009
-import { getCookie } from 'typescript-cookie';
 import { session } from '../session';
 
 const API_URL = process.env.API_URL;
@@ -47,7 +27,7 @@ export async function load({ cookies }) {
 		return { user: null, login: false }; // ou un autre traitement approprié
 	}
 }
->>>>>>> 5760273087ee00868df01f802a01aa99ccabf680
+
 
 export const actions = {
 	login: async ({ cookies, request, url }) => {
@@ -62,26 +42,12 @@ export const actions = {
 		} catch (error) {
 			return fail(400, { email, missing: true, errorEmail: 'Password and/or email is invalid !' });
 		}
-		const user = await db.getUser(email);
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-		if (user || user.password !== hash(password)) {
-			return fail(400, { email, incorrect: true });
-		}
-
-		cookies.set('sessionid', await db.createSession(user), { path: '/' });
-
-		if (url.searchParams.has('redirectTo')) {
-			redirect(303, url.searchParams.get('redirectTo'));
-=======
 		const user = (await fetch(`${API_URL}/user/email/${email}`)).json();
 		console.log(await user);
 		const userString = JSON.stringify(await user);
 		cookies.set('user', userString, { path: '/' });
 
-=======
->>>>>>> f0a9c7614cc40dd07c1118b5892b820b48567009
 		try {
 			// Faites une requête d'authentification au backend (par exemple, avec fetch ou axios)
 			const response = await fetch(`${API_URL}/auth/login`, {
@@ -113,16 +79,14 @@ export const actions = {
 		} catch (error) {
 			console.error('Error during login:', error);
 			return null;
->>>>>>> 5760273087ee00868df01f802a01aa99ccabf680
+
 		}
 	}
 	return { success: true };
 } satisfies Actions;
 
-<<<<<<< HEAD
-=======
+
 export function _getToken(): string | null {
 	getCookie('user');
 	return localStorage.getItem('token');
 }
->>>>>>> 5760273087ee00868df01f802a01aa99ccabf680
